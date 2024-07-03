8920450d3ba7b8f9abce48573446bced6b4276f2
Vipul Dubey <78210934+VV244@users.noreply.github.com>
2024-07-03 10:53:24 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,22 @@
+# Simple Calculator 
+
+num1 = int(input("Enter the first number :"))
+num2 = int(input("Enter the second number :"))
+
+sum = num1 + num2
+difference = num1 - num2
+product = num1 * num2
+division = num1 /  num2 
+floordivision = num1 // num2 
+modulus = num1 % num2
+exponentiation = num1 ** num2
+
+
+
+print("Sum:" , sum) 
+print("Difference:" , difference)
+print("Product:" , product)
+print("Division:" , division)
+print("Floor Division:" , floordivision)
+print("Modulus:" , modulus)
+print("Exponentiation" , exponentiation)
